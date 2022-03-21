912b4ee14275127859039f8ea5e2e6f93d8337b3
Jeison Azevedo <j3ison@gmail.com>
2022-03-20 23:11:17 -0300

    feat: add validation for project with same name

--- src/modules/projects/updateProject.js
+++ src/modules/projects/updateProject.js
@@ -7,12 +7,25 @@ function validateParams(projectName) {
   }
 }
 
-module.exports = async ({ id, projectName, userId }) => {
-  validateParams(projectName);
+async function validateProjectExists(projectName, userId) {
+  const project = await findOneProject({ projectName, userId });
+  if (project) {
+    throw new AppError('There`s already a project with this name.');
+  }
+}
+
+async function validateProjectNameDoesNotExists(id, userId) {
   const project = await findOneProject({ id, userId });
   if (!project) {
-    throw Error('Project doesn`t exists.');
+    throw new AppError('Project doesn`t exists.');
   }
+}
+
+module.exports = async ({ id, projectName, userId }) => {
+  validateParams(projectName);
+  await validateProjectExists(id, userId);
+  await validateProjectNameDoesNotExists(projectName, userId);
+
   await updateOneProject({ id }, { projectName });
   return findOneProject({ id });
 };
